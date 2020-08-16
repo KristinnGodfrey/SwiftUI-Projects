1fea419e585fe61c7ebf4f896a9d554e8352df49
kristinngodfrey <kristinngodfrey@gmail.com>
2020-08-16 23:01:24 +0000

    fin

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,15 +9,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
-
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreText = ""
+    @State private var userScore = 0
+    
     var body: some View {
-        Button("Show Alert") {
-            self.showingAlert = true
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            Text("")
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                    
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Text("Score: \(userScore)")
+                Spacer()
+            }
+            
+                .foregroundColor(.white )
+            .alert(isPresented: $showingScore) {
+                Alert(title: Text(scoreTitle), message: Text("\(scoreText)"), dismissButton: .default(Text("Continue")) {
+                    self.askQuestion()
+                    })
+            }
         }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+    }
+    func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreText = "Good job"
+            userScore += 1
+        } else {
+            scoreTitle = "Wrong"
+        scoreText = "The flag you pressed is \(countries[number])"
         }
+        
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GuessTheFlag
+## Project Details
+This is my second Swift project, code can be found in [ContentView.swift](https://github.com/KristinnGodfrey/WeSplit/blob/master/GuessTheFlag/ContentView.swift
+
+## Simulator
+<p align="left">
+  <img src="/p1.png" width="30%" /> 
+</p>
+<p align="left">
+  <img src="/p2.png" width="30%" /> 
+  <img src="/p3.png" width="30%" /> 
+</p>
