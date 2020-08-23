a0f7e47e5be8647e666b1bedf01faf202b5c1079
kristinngodfrey <kristinngodfrey@gmail.com>
2020-08-23 13:12:17 +0000

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Kristinn Godfrey on 23/08/2020.
+//  Copyright © 2020 me.kristinn.godfrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
